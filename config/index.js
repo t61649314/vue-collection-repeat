8ae0a9b461e442eefd021715c87cb79f4ce2114f
田放 <tianfanga@zbj.com>
2018-09-03 15:02:30 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/dist/',
+    assetsPublicPath: '/vue-collection-repeat/dist/',
 
     /**
      * Source Maps
